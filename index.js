455b4cee64d42c0e9877cfc0d40dfad230495603
Tai Studio <tai.studio@outlook.fr>
2022-02-22 17:20:41 +0100

    Update

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+const fs = require('fs');
 const dotenv = require('dotenv');
 const child_process = require('child_process');
 var pathToFfmpeg = require('ffmpeg-static');
 
 dotenv.config();
+
+if(fs.existsSync(process.env.outGIF)){
+    fs.rmSync(process.env.outGIF);
+}
 
 child_process.exec(`${pathToFfmpeg} -i ${process.env.inMP4} ${process.env.outGIF}`, (err, stdout) => {
     console.log(err, stdout);
